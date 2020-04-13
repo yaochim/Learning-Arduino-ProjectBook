81107f260ca8019022200d7d130506e83813342a
Yao on Ryzen Win Desktop <yaochim8@gmail.com>
2020-04-14 00:03:57 +0100

    Arduino Official Projects Book - Project 7
    beware of second code line, its not needed.

---
+++ sketch_ard_proj7/sketch_ard_proj7.ino
@@ -0,0 +1,29 @@
+int buttons[6];
+
+// int buttons[0] = 2;
+
+int notes[] = {262,294,330,349};
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int keyVal = analogRead(A0);
+  Serial.println(keyVal);
+
+  if (keyVal >= 690 && keyVal <= 700){
+    tone(8, notes[0]);
+  } else if (keyVal >= 990 && keyVal <= 1010){
+    tone(8, notes[1]);
+  } else if (keyVal >= 505 && keyVal <= 515){
+    tone(8, notes[2]);
+  } else if (keyVal >= 5 && keyVal <= 10){
+    tone(8, notes[3]);
+  } else {
+    noTone(8);
+  }
+}
