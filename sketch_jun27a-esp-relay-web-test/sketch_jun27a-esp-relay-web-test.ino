f3ddb15795e763c9892cda47e565ddbcb20999ee
Yao on Flex-Ryxen-MXnix <yaochim8@gmail.com>
2020-06-29 23:24:42 +0100

    put relay on timer if activated

--- sketch_jun27a-esp-relay-web-test/sketch_jun27a-esp-relay-web-test.ino
+++ sketch_jun27a-esp-relay-web-test/sketch_jun27a-esp-relay-web-test.ino
@@ -150,5 +150,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(digitalRead(relayGPIOs[0]) == 1){
+    sleep(10);
+    digitalWrite(relayGPIOs[0], LOW);
+  }
 
 }
